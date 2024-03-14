2fdb441fa605c7721f3597757495ff364af4bf3f
pawina405 <122677661+pawina405@users.noreply.github.com>
2024-03-14 14:40:38 +0700

    Add files via upload

--- back03/app.js
+++ back03/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const notFound = require('./middlewares/notFound')
 const errorMiddleware = require('./middlewares/error')
 const authRoute = require('./routes/auth-route')
+const bookRoute = require('./routes/book-route')
 
 
 const app = express()
@@ -13,6 +14,7 @@ app.use(express.json())
 
 // service
 app.use('/auth', authRoute)
+app.use('/book', bookRoute)
 
 
 // notFound
